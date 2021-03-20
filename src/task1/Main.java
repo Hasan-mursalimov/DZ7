a2a06aef05e43560820c0f3e55bcf41089e64755
hasan <hasan1993mur@gmail.com>
2021-03-20 17:02:24 +0500

    Вариант первый

---
+++ src/task1/Main.java
@@ -0,0 +1,66 @@
+package task1;
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    static String data = "Мы шли через реку голопом";
+    ///преобразую строку в массив , для дальнейшего добавления в list
+    static String[] strArr = data.split(" ");
+    ///массив для добавления в метод output
+    static byte[] dataByte;
+    //создаю ArrayList для более удобной сортировки
+    static List<String> list = new ArrayList<>();
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+
+        output("Мы шли через реку голопом", "Hello.txt");
+        input("Hello.txt");
+
+    }
+
+    public static void input(String fileName) {
+        System.out.println("Содиржимое файла: ");
+        try (FileInputStream fileInputStream = new FileInputStream(fileName)) {
+            int tmp = -1;
+            while ((tmp = fileInputStream.read()) != -1) {
+                System.out.println((char) tmp);
+            }
+        } catch (
+                FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void output(String data, String fileName) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+            // преобразую массив String в коллекцию
+            list = Arrays.asList(strArr);
+            //сортирую коллекцию
+            Collections.sort(list);
+
+            String str = String.join(" ", list);
+
+
+            dataByte = str.getBytes();
+
+            fileOutputStream.write(dataByte);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+
+
+
+
